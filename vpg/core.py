5e79dc415a23170ea721a8458d802b7b30793e18
Burak Turgut <turgutburak0144@gmail.com>
2024-02-27 18:21:41 +0300

    update

--- vpg/core.py
+++ vpg/core.py
@@ -1,10 +1,13 @@
 import numpy as np
 import scipy.signal
-import gym.spaces import Box, Discrete
+from gym.spaces import Box, Discrete
 
 import torch
 import torch.nn as nn
 from torch.distributions.normal import Normal
 from torch.distributions.categorical import Categorical
 
-def 
+def combined_shape(length, shape=None):
+    if shape is None:
+        return (length,)
+    return (length, shape) if np.isscalar(shape) else (length, *shape)
